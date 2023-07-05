fb4577d1f3225f656b95d60dfb330f019f9db6f6
nimarya <masha.nichiporenko@gmail.com>
2023-07-05 13:19:51 +0300

    added posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Home') }}
+            {{ __('Posts') }}
         </h2>
     </x-slot>
 
@@ -9,10 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
                 @foreach ($posts as $post)
-                    <div class="font-bold">{{ $post->header; }}</div>
-                    <div>{{ $post->body; }}</div>
-                    <div class="mt-4 basis-1/6">
-                        <img class="rounded-lg h-60 float-left" src="{{ '/images/'.$post->image }}" alt="image description">
+                    <div>     
+                        <div class="mt-4 basis-1/6">
+                            <img class="rounded-lg h-60 float-left" src="{{ '/images/'.$post->image }}" alt="image description">
+                        </div>
+                        <div class="font-bold">{{ $post->header; }}</div>
                     </div>
                 @endforeach
             </div>
